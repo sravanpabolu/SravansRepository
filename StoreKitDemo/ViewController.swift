f21c29114d88f6c4a619f0030b3ae5c7ddd90f34
Sravan <sravan@Sravans-MacBook-Pro.local>
2015-08-22 17:44:10 +0530

    Initial Commit

---
+++ StoreKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreKitDemo
+//
+//  Created by Sravan on 22/08/15.
+//  Copyright (c) 2015 Sravan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
