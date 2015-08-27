5569c85eabff604d5511364559b5869fcf18ede1
Sravan Kumar Pabolu <sravanpabolu@gmail.com>
2015-08-27 23:37:00 +0530

    StoreKit : Initial commit

--- StoreKitDemo/ViewController.swift
+++ StoreKitDemo/ViewController.swift
@@ -7,9 +7,33 @@
 //
 
 import UIKit
+import StoreKit 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SKStoreProductViewControllerDelegate {
+    
+    /*
+    let itunesIdentifier = "http://itunes.apple.com/us/movie/the-pirates!-band-of-misfits/id527049179"
 
+    http://www.techotopia.com/index.php/Making_Store_Purchases_with_the_SKStoreProductViewController_Class
+
+    
+    let storeViewController = SKStoreProductViewController()
+    storeViewController.delegate = self
+    
+    let parameters = [SKStoreProductParameterITunesItemIdentifier :
+    NSNumber(integer: 676059878)]
+    
+    storeViewController.loadProductWithParameters(parameters,
+    completionBlock: {result, error in
+    if result {
+    self.presentViewController(storeViewController,
+    animated: true, completion: nil)
+    }
+    
+    })
+
+    */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +44,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateAppBtnTapped(sender: AnyObject) {
+        let parameters = [SKStoreProductParameterITunesItemIdentifier: String("333700869")]
+        let spvc = SKStoreProductViewController() ;
+        spvc.delegate = self
+        spvc.loadProductWithParameters(parameters, completionBlock: nil)
+        self.presentViewController(spvc, animated: true, completion: nil)
+    }
+    
+    func productViewControllerDidFinish(viewController: SKStoreProductViewController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
 
